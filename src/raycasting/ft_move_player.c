dc49b540df3adee5b021245d6d732e18710fe569
Alex Barbosa Felix Da Silva <alebarbo@c2r5s10.42porto.com>
2026-04-15 15:25:18 +0100

    added safeguard to character movement, but crashes at corners still happen

--- src/raycasting/ft_move_player.c
+++ src/raycasting/ft_move_player.c
@@ -6,7 +6,7 @@
 /*   By: alebarbo <alebarbo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 17:57:26 by alebarbo          #+#    #+#             */
-/*   Updated: 2026/04/14 19:47:13 by alebarbo         ###   ########.fr       */
+/*   Updated: 2026/04/15 15:20:28 by alebarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,58 +16,94 @@ void	ft_move_left(t_data *dt)
 {
 	int		new_pos_x;
 	int		new_pos_y;
+	double	pos_x;
+	double	pos_y;
 
 	new_pos_x = (int)(dt->player.pos_x
 		- (dt->player.plane_x * ceil(dt->player.speed)));
 	new_pos_y = (int)(dt->player.pos_y
 		- (dt->player.plane_y * ceil(dt->player.speed)));
+	pos_x = dt->player.pos_x;
+	pos_y = dt->player.pos_y;
 	if (dt->map.grid[(int)dt->player.pos_y][new_pos_x] != '1')
-		dt->player.pos_x -= (dt->player.speed * dt->player.plane_x);
+		pos_x = dt->player.pos_x - (dt->player.speed * dt->player.plane_x);
 	if (dt->map.grid[new_pos_y][(int)dt->player.pos_x] != '1')
-		dt->player.pos_y -= (dt->player.speed * dt->player.plane_y);
+		pos_y = dt->player.pos_y - (dt->player.speed * dt->player.plane_y);
+	if (dt->map.grid[(int)pos_y][(int)pos_x] != '1')
+	{
+		dt->player.pos_x = pos_x;
+		dt->player.pos_y = pos_y;
+	}
 }
 
 void	ft_move_right(t_data *dt)
 {
 	int		new_pos_x;
 	int		new_pos_y;
+	double	pos_x;
+	double	pos_y;
 
 	new_pos_x = (int)(dt->player.pos_x
 		+ (dt->player.plane_x * ceil(dt->player.speed)));
 	new_pos_y = (int)(dt->player.pos_y
 		+ (dt->player.plane_y * ceil(dt->player.speed)));
+	pos_x = dt->player.pos_x;
+	pos_y = dt->player.pos_y;
 	if (dt->map.grid[(int)dt->player.pos_y][new_pos_x] != '1')
-		dt->player.pos_x += (dt->player.speed * dt->player.plane_x);
+		pos_x = dt->player.pos_x + (dt->player.speed * dt->player.plane_x);
 	if (dt->map.grid[new_pos_y][(int)dt->player.pos_x] != '1')
-		dt->player.pos_y += (dt->player.speed * dt->player.plane_y);
+		pos_y = dt->player.pos_y + (dt->player.speed * dt->player.plane_y);
+	if (dt->map.grid[(int)pos_y][(int)pos_x] != '1')
+	{
+		dt->player.pos_x = pos_x;
+		dt->player.pos_y = pos_y;
+	}
 }
 
 void	ft_move_forward(t_data *dt)
 {
 	int		new_pos_x;
 	int		new_pos_y;
+	double	pos_x;
+	double	pos_y;
 
 	new_pos_x = (int)(dt->player.pos_x
 		+ (dt->player.dir_x * ceil(dt->player.speed)));
 	new_pos_y = (int)(dt->player.pos_y
 		+ (dt->player.dir_y * ceil(dt->player.speed)));
+	pos_x = dt->player.pos_x;
+	pos_y = dt->player.pos_y;
 	if (dt->map.grid[(int)dt->player.pos_y][new_pos_x] != '1')
-		dt->player.pos_x += (dt->player.speed * dt->player.dir_x);
+		pos_x = dt->player.pos_x + (dt->player.speed * dt->player.dir_x);
 	if (dt->map.grid[new_pos_y][(int)dt->player.pos_x] != '1')
-		dt->player.pos_y += (dt->player.speed * dt->player.dir_y);
+		pos_y = dt->player.pos_y + (dt->player.speed * dt->player.dir_y);
+	if (dt->map.grid[(int)pos_y][(int)pos_x] != '1')
+	{
+		dt->player.pos_x = pos_x;
+		dt->player.pos_y = pos_y;
+	}
 }
 
 void	ft_move_backward(t_data *dt)
 {
 	int		new_pos_x;
 	int		new_pos_y;
+	double	pos_x;
+	double	pos_y;
 
 	new_pos_x = (int)(dt->player.pos_x
 		- (dt->player.dir_x * ceil(dt->player.speed)));
 	new_pos_y = (int)(dt->player.pos_y
 		- (dt->player.dir_y * ceil(dt->player.speed)));
+	pos_x = dt->player.pos_x;
+	pos_y = dt->player.pos_y;
 	if (dt->map.grid[(int)dt->player.pos_y][new_pos_x] != '1')
-		dt->player.pos_x -= (dt->player.speed * dt->player.dir_x);
+		pos_x = dt->player.pos_x - (dt->player.speed * dt->player.dir_x);
 	if (dt->map.grid[new_pos_y][(int)dt->player.pos_x] != '1')
-		dt->player.pos_y -= (dt->player.speed * dt->player.dir_y);
+		pos_y = dt->player.pos_y - (dt->player.speed * dt->player.dir_y);
+	if (dt->map.grid[(int)pos_y][(int)pos_x] != '1')
+	{
+		dt->player.pos_x = pos_x;
+		dt->player.pos_y = pos_y;
+	}
 }
