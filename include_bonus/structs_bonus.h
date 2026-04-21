20ec26274a241500bdb23aa7e5edbd4acf0a7c3b
Lucca Cruz <lucca1609.ls@gmail.com>
2026-04-21 13:03:44 +0100

    fix: norm errors and add 42 header.

--- include_bonus/structs_bonus.h
+++ include_bonus/structs_bonus.h
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structs_bonus.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lseabra- <lseabra-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/21 13:02:10 by lseabra-          #+#    #+#             */
+/*   Updated: 2026/04/21 13:03:14 by lseabra-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef STRUCTS_BONUS_H
 # define STRUCTS_BONUS_H
 
 # include "cub3d.h"
 
-typedef	struct s_minimap
+typedef struct s_minimap
 {
 	int		x;
 	int		y;
@@ -20,4 +32,5 @@ typedef	struct s_minimap
 	t_rgb	color_block;
 	t_rgb	color_player;
 }	t_minimap;
+
 #endif
