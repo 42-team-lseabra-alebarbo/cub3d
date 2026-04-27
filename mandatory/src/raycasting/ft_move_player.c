560b1669641fc31734bf6c4fe585cfde71263306
Magusk Lutus <26604686-Magusk89@users.noreply.gitlab.com>
2026-04-27 21:34:15 +0100

    fix: fixed player movement to correctly approach the walls

--- mandatory/src/raycasting/ft_move_player.c
+++ mandatory/src/raycasting/ft_move_player.c
@@ -6,7 +6,7 @@
 /*   By: alebarbo <alebarbo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 17:57:26 by alebarbo          #+#    #+#             */
-/*   Updated: 2026/04/15 22:52:59 by alebarbo         ###   ########.fr       */
+/*   Updated: 2026/04/27 21:33:08 by alebarbo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,60 +14,92 @@
 
 void	ft_move_left(t_data *dt)
 {
-	int		new_pos_x;
-	int		new_pos_y;
+	double	new_pos_x;
+	double	new_pos_y;
 
-	new_pos_x = (int)(dt->player.pos_x
-			- (dt->player.plane_x * ceil(SPEED)));
-	new_pos_y = (int)(dt->player.pos_y
-			- (dt->player.plane_y * ceil(SPEED)));
-	if (dt->map.grid[(int)dt->player.pos_y][new_pos_x] != '1')
-		dt->player.pos_x -= (SPEED * dt->player.plane_x);
-	if (dt->map.grid[new_pos_y][(int)dt->player.pos_x] != '1')
-		dt->player.pos_y -= (SPEED * dt->player.plane_y);
+	new_pos_x = dt->player.pos_x
+			- (dt->player.plane_x * SPEED);
+	new_pos_y = dt->player.pos_y
+			- (dt->player.plane_y * SPEED);
+	while (dt->map.grid[(int)dt->player.pos_y][(int)(new_pos_x + 0.25)] == '1')
+		new_pos_x -= 0.01;
+	while (dt->map.grid[(int)dt->player.pos_y][(int)(new_pos_x - 0.25)] == '1')
+		new_pos_x += 0.01;
+	while (dt->map.grid[(int)(new_pos_y + 0.25)][(int)dt->player.pos_x] == '1')
+		new_pos_y -= 0.01;
+	while (dt->map.grid[(int)(new_pos_y - 0.25)][(int)dt->player.pos_x] == '1')
+		new_pos_y += 0.01;
+	if (dt->map.grid[(int)dt->player.pos_y][(int)new_pos_x] != '1')
+		dt->player.pos_x = new_pos_x;
+	if (dt->map.grid[(int)new_pos_y][(int)dt->player.pos_x] != '1')
+		dt->player.pos_y = new_pos_y;
 }
 
 void	ft_move_right(t_data *dt)
 {
-	int		new_pos_x;
-	int		new_pos_y;
+	double	new_pos_x;
+	double	new_pos_y;
 
-	new_pos_x = (int)(dt->player.pos_x
-			+ (dt->player.plane_x * ceil(SPEED)));
-	new_pos_y = (int)(dt->player.pos_y
-			+ (dt->player.plane_y * ceil(SPEED)));
-	if (dt->map.grid[(int)dt->player.pos_y][new_pos_x] != '1')
-		dt->player.pos_x += (SPEED * dt->player.plane_x);
-	if (dt->map.grid[new_pos_y][(int)dt->player.pos_x] != '1')
-		dt->player.pos_y += (SPEED * dt->player.plane_y);
+	new_pos_x = dt->player.pos_x
+			+ (dt->player.plane_x * SPEED);
+	new_pos_y = dt->player.pos_y
+			+ (dt->player.plane_y * SPEED);
+	while (dt->map.grid[(int)dt->player.pos_y][(int)(new_pos_x + 0.25)] == '1')
+		new_pos_x -= 0.01;
+	while (dt->map.grid[(int)dt->player.pos_y][(int)(new_pos_x - 0.25)] == '1')
+		new_pos_x += 0.01;
+	while (dt->map.grid[(int)(new_pos_y + 0.25)][(int)dt->player.pos_x] == '1')
+		new_pos_y -= 0.01;
+	while (dt->map.grid[(int)(new_pos_y - 0.25)][(int)dt->player.pos_x] == '1')
+		new_pos_y += 0.01;
+	if (dt->map.grid[(int)dt->player.pos_y][(int)new_pos_x] != '1')
+		dt->player.pos_x = new_pos_x;
+	if (dt->map.grid[(int)new_pos_y][(int)dt->player.pos_x] != '1')
+		dt->player.pos_y = new_pos_y;
 }
 
 void	ft_move_forward(t_data *dt)
 {
-	int		new_pos_x;
-	int		new_pos_y;
+	double	new_pos_x;
+	double	new_pos_y;
 
-	new_pos_x = (int)(dt->player.pos_x
-			+ (dt->player.dir_x * ceil(SPEED)));
-	new_pos_y = (int)(dt->player.pos_y
-			+ (dt->player.dir_y * ceil(SPEED)));
-	if (dt->map.grid[(int)dt->player.pos_y][new_pos_x] != '1')
-		dt->player.pos_x += (SPEED * dt->player.dir_x);
-	if (dt->map.grid[new_pos_y][(int)dt->player.pos_x] != '1')
-		dt->player.pos_y += (SPEED * dt->player.dir_y);
+	new_pos_x = dt->player.pos_x
+			+ (dt->player.dir_x * SPEED);
+	new_pos_y = dt->player.pos_y
+			+ (dt->player.dir_y * SPEED);
+	while (dt->map.grid[(int)dt->player.pos_y][(int)(new_pos_x + 0.25)] == '1')
+		new_pos_x -= 0.01;
+	while (dt->map.grid[(int)dt->player.pos_y][(int)(new_pos_x - 0.25)] == '1')
+		new_pos_x += 0.01;
+	while (dt->map.grid[(int)(new_pos_y + 0.25)][(int)dt->player.pos_x] == '1')
+		new_pos_y -= 0.01;
+	while (dt->map.grid[(int)(new_pos_y - 0.25)][(int)dt->player.pos_x] == '1')
+		new_pos_y += 0.01;
+	if (dt->map.grid[(int)dt->player.pos_y][(int)new_pos_x] != '1')
+		dt->player.pos_x = new_pos_x;
+	if (dt->map.grid[(int)new_pos_y][(int)dt->player.pos_x] != '1')
+		dt->player.pos_y = new_pos_y;
 }
 
 void	ft_move_backward(t_data *dt)
 {
-	int		new_pos_x;
-	int		new_pos_y;
+	double	new_pos_x;
+	double	new_pos_y;
 
-	new_pos_x = (int)(dt->player.pos_x
-			- (dt->player.dir_x * ceil(SPEED)));
-	new_pos_y = (int)(dt->player.pos_y
-			- (dt->player.dir_y * ceil(SPEED)));
-	if (dt->map.grid[(int)dt->player.pos_y][new_pos_x] != '1')
-		dt->player.pos_x -= (SPEED * dt->player.dir_x);
-	if (dt->map.grid[new_pos_y][(int)dt->player.pos_x] != '1')
-		dt->player.pos_y -= (SPEED * dt->player.dir_y);
+	new_pos_x = dt->player.pos_x
+			- (dt->player.dir_x * SPEED);
+	new_pos_y = dt->player.pos_y
+			- (dt->player.dir_y * SPEED);
+	while (dt->map.grid[(int)dt->player.pos_y][(int)(new_pos_x + 0.25)] == '1')
+		new_pos_x -= 0.01;
+	while (dt->map.grid[(int)dt->player.pos_y][(int)(new_pos_x - 0.25)] == '1')
+		new_pos_x += 0.01;
+	while (dt->map.grid[(int)(new_pos_y + 0.25)][(int)dt->player.pos_x] == '1')
+		new_pos_y -= 0.01;
+	while (dt->map.grid[(int)(new_pos_y - 0.25)][(int)dt->player.pos_x] == '1')
+		new_pos_y += 0.01;
+	if (dt->map.grid[(int)dt->player.pos_y][(int)new_pos_x] != '1')
+		dt->player.pos_x = new_pos_x;
+	if (dt->map.grid[(int)new_pos_y][(int)dt->player.pos_x] != '1')
+		dt->player.pos_y = new_pos_y;
 }
