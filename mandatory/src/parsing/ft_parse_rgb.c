2768b9a31bbca3d07ad98c61e83c160b0a42ce38
Lucca Cruz <lucca1609.ls@gmail.com>
2026-04-28 14:25:08 +0100

    fix: rgb parsing accepting a non digit in the end.

--- mandatory/src/parsing/ft_parse_rgb.c
+++ mandatory/src/parsing/ft_parse_rgb.c
@@ -6,7 +6,7 @@
 /*   By: lseabra- <lseabra-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/23 10:29:46 by lseabra-          #+#    #+#             */
-/*   Updated: 2026/04/27 17:47:16 by lseabra-         ###   ########.fr       */
+/*   Updated: 2026/04/28 14:23:13 by lseabra-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,8 @@ static t_result	ft_atouc_rgb(t_surface *surf, t_u_char *component, size_t *i)
 		return (ft_p_err_ret("ft_atouc_rgb()", NULL, ERR_MISS_PARAM));
 	else if (surf->rgb_str[*i] == '\0')
 		return (ft_p_err_ret(NULL, ERR_MISS_PARAM, ERR_MISS_RGB_COMP));
-	else if (surf->rgb_str[*i] == '-')
+	else if (ft_is_digit(surf->rgb_str[*i]) == FALSE)
 		return (ft_p_err_ret(NULL, surf->rgb_str, ERR_INV_RGB));
-	else if (surf->rgb_str[*i] == '+')
-		(*i)++;
 	res = 0;
 	while (surf->rgb_str[*i] && ft_is_digit(surf->rgb_str[*i]) == TRUE)
 	{
@@ -63,7 +61,6 @@ t_result	ft_parse_rgb(t_surface *surface)
 	i++;
 	if (ft_atouc_rgb(surface, &surface->rgb.b, &i) != SUCCESS)
 		return (FAILURE);
-	i++;
 	while (ft_is_space(surface->rgb_str[i]))
 		i++;
 	if (surface->rgb_str[i])
